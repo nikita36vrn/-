cecca3a583144e7bf81393443332ead2e7b17556
w3nsent <andreytheslayer@gmail.com>
2019-06-26 17:03:51 +0300

    First_dayMax

---
+++ src/main/frontend/src/app/header/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import mainIcon from '../../../public/static/images/icons/cat_slippers.png'
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <header className="ui container header_padded">
+                <img className="ui middle aligned mini image" src={mainIcon}/>
+                <span className="blue_cat">Кот</span><span className="red_cat">услуги</span>
+            </header>
+        )
+    }
+}
