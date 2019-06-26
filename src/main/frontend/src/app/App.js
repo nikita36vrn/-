89c770636ec4ff8e0cc4094a157204241a860c80
YourOwnTenshi <BulgakovNS@ya.ru>
2019-06-26 17:06:46 +0300

    the 3rd day of practice in "RTLabs"

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader/root'
 import "./style.css"
+import Header from "./header/header";
+import Main from "./main/main";
+import Footer from "./footer/footer";
+import "semantic-ui-css/semantic.min.css"
 
 class App extends Component {
 
     render() {
-        return <div>Тест</div>
+        return (
+            <div className="ui container">
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        )
     }
 }
 
