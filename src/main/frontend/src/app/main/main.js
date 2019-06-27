961d35f9984a0eea088e98c3b12700bc781b176f
karina <karina.nitchenko@yandex.ru>
2019-06-27 09:23:46 +0300

    karina

--- src/main/frontend/src/app/main/main.js
+++ src/main/frontend/src/app/main/main.js
@@ -1,39 +1,47 @@
 import React, {Component} from 'react';
 import axios from "axios";
-
+import {icons} from "../constants";
 
 
 export default class Main extends Component {
 
     state = {
-        serviceList: []
+        listService: []
     };
 
-
-    getServiceList = () => {
-
-        axios.post('/api/listService')
-            .then((response) => {
-                this.setState({serviceList: response.data.content})
-            })
-            // console.log(response.data);})//если удачно выпол ,вернется аргумент и пойдет ответ функции
-            .catch((error) => {
-                console.log(error);// если ошибка ,сработает catch
-            });
-    };
 
     componentDidMount() {
-
-
         this.getServiceList()
-    }//вызов функции
-
-    render() {
-            return (<div className="ui cards">
-                {this.state.serviceList.map((item) => {
-                return ( <div className="ui card" key={item.id}>{item.name}</div>)
-                })}</div>)
     }
 
-};
+    getServiceList = () => {
+        axios.post('/api/listService')
+            .then((response) => {
+                this.setState({listService: response.data.content})
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    };
 
+    render() {
+        return <div className="ui three cards">{
+            this.state.listService.map((item) => {
+                return (
+                    <div className= "ui card" key={item.id}>
+                    <div className="content">
+                    <img
+                src={icons[item.id]}
+                className="ui mini right floated image"
+                    />
+                    <div className="header">{item.name}</div>
+                    <div className="meta">{item.categories.map((item)=>{return item.name}).join(',')}</div>
+                    <div className="description">  {item.description} </div>
+                    </div>
+                    </div>
+            )
+            })
+        }</div>
+    }
+
+}
