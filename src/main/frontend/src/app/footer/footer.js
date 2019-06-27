060f0c31d8502aaa71e69d78aa96af274c9bf165
Ksenobyte <nik.pavel1999@gmail.com>
2019-06-27 19:32:28 +0300

    Day 2.0

--- src/main/frontend/src/app/footer/footer.js
+++ src/main/frontend/src/app/footer/footer.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 
 export default class Footer extends Component {
+
     render() {
         return (
-            <footer className="ui container footer_padded">
-                <span className="up">Пыщь-Пыщь ЖЫВТОНЕ Чо-чо Упячка ЖжжЖЖжж... Уропопяка ГОЛАКТЕКО ВОПАСНОСТЕ!!!</span>
+            <footer className="footer">
+                <div className="ui container up footer_padded">
+                    Пыщь-Пыщь ЖЫВТОНЕ Чо-чо Упячка ЖжжЖЖжж... Уропопяка ГОЛАКТЕКО ВОПАСНОСТЕ!!!
+                </div>
             </footer>
+
         )
     }
+
 }
--- src/main/frontend/src/app/main/main.js
+++ src/main/frontend/src/app/main/main.js
@@ -19,25 +19,42 @@ export default class Main extends Component {
     }
 
     render() {
-        return <div className="ui three cards">
-
-            {this.state.serviceList.map((item) => {
-
-            return (
-                <div className="ui card" key={item.id}>
-                    <div className="content">
-                        <img
-                            src={icons[item.id]}
-                            className="ui tiny right floated image"
-                            alt={"Pic"}
-                        />
-                        <div className="header">{item.name}</div>
-                        <div className="meta">{item.categories.map((item)=>{return item.name}).join(',') }</div>
-                        <div className="description">{item.description}</div>
-                    </div>
+        return (
+            <div>
+                <div style={{paddingBottom: 15, paddingTop: 15}}>
+                    <Input
+                        placeholder='Поиск...'
+                        icon="stop"
+                        fluid
+                        focus
+                        onChange={(evt, val) => {
+                            this.search(val);
+                        }}
+                    />
                 </div>
-                )
-            })
-            }</div>
+                {this.state.listServiceFiltered.length ?
+                    <div className="ui three cards">{
+                        this.state.listServiceFiltered.map((item) => {
+                            return (
+                                <Link to={'/'+ item.id} className="ui  card" key={item.id}>
+                                    <div className="content">
+                                        <img
+                                            src={icons[item.id]}
+                                            className="ui mini right floated image"
+                                        />
+                                        <div className="header">{item.name}</div>
+                                        <div className="meta">{item.categories.map((item) => {
+                                            return item.name
+                                        }).join(',')}</div>
+                                        <div className="description">{item.description} </div>
+                                    </div>
+                                </Link>
+                            )
+                        })
+                    }</div>
+                    : <div>Ничего не найдено</div>}
+
+            </div>
+        )
     }
 };
