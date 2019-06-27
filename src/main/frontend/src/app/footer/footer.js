22cabeda46650c3bdecf1be566495b482f8de695
Ksenobyte <nik.pavel1999@gmail.com>
2019-06-27 20:26:00 +0300

    Day 2.2

--- src/main/frontend/src/app/footer/footer.js
+++ src/main/frontend/src/app/footer/footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                <div className="ui container up footer_padded">
+                <div className="ui container footer_padded">
                     Пыщь-Пыщь ЖЫВТОНЕ Чо-чо Упячка ЖжжЖЖжж... Уропопяка ГОЛАКТЕКО ВОПАСНОСТЕ!!!
                 </div>
             </footer>
