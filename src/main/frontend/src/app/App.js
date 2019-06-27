d4746f381c798822cdabac1ca11fa9d714e872bf
Ksenobyte36 <Ksenobyte36>
2019-06-27 12:46:25 +0400

    Day 2 (Popov)

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import {hot} from 'react-hot-loader/root'
 import "./style.css"
 import Header from "./header/header";
-import Main from "./main/main";
+import ServiceList from "./serviceList/serviceList";
 import 'semantic-ui-css/semantic.min.css'
+import serviceForm from "./serviceForm/serviceForm";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Page404 from "./404/404";
 
 class App extends Component {
 
@@ -11,7 +14,13 @@ class App extends Component {
         return (
             <div className="ui container">
                 <Header/>
-                <Main/>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={ServiceList} />
+                        <Route path="/1" exact component={serviceForm} />
+                        <Route component={Page404}/>
+                    </Switch>
+                </Router>
             </div>
         )
     }
---
+++ src/main/frontend/src/app/serviceForm/serviceForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Stepper from "./Stepper";
+
+export default class serviceForm extends Component{
+
+    render(){
+        return (<div>
+            <Stepper activeStepNumber={1}/>
+        </div>)
+    }
+
+}
+
