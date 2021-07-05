834d9c4ce916ce7918f19323a9a5fe4a76e7dd61
Maxim Komov <komovmaximqa@gmail.com>
2021-07-05 21:15:49 +0300

    Разработка backend

--- src/main/java/ru/praktika/kotouslugi/util/FunctionSE.java
+++
@@ -1,9 +0,0 @@
-package ru.praktika.kotouslugi.util;
-
-import ru.praktika.kotouslugi.exception.ServiceException;
-
-@FunctionalInterface
-public interface FunctionSE<T>  {
-    T apply(T t) throws ServiceException;
-}
-
