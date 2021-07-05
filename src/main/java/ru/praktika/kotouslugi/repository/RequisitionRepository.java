c50b78083106764a66077b0142afd760e5490972
Maxim Komov <komovmaximqa@gmail.com>
2021-07-05 21:06:10 +0300

    Разработка backend

--- src/main/java/ru/praktika/kotouslugi/repository/RequisitionRepository.java
+++ src/main/java/ru/praktika/kotouslugi/repository/RequisitionRepository.java
@@ -1,4 +1,4 @@
-package ru.praktika.kotouslugi.dao;
+package ru.praktika.kotouslugi.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import ru.praktika.kotouslugi.model.Requisition;
