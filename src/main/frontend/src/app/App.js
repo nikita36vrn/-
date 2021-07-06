91950bd69486a32cba4d48c815f31941ce433ee3
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2021-07-06 11:04:03 +0300

    dayTwo

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader/root'
 import "./style.css"
+import CatalogPage from "./Catalog/CatalogPage";
+import "semantic-ui-css/semantic.min.css"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Page404 from "./Page404/Page404";
+import HeaderPage from "./Header/HeaderPage";
 
 class App extends Component {
 
     render() {
-        return <div>Тест</div>
+        return <div className='ui container'>
+
+            <HeaderPage/>
+
+            <BrowserRouter>
+                <Switch>
+                    <Route path={"/"} exact component={CatalogPage}/>
+                    <Route component={Page404}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
     }
 }
 
