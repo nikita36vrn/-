979b4c488996b0cec3e54fac6ec91a53a7330dd2
Marina_2.0 <chursina0121@gmail.com>
2022-01-28 18:15:17 +0300

    Time

--- src/main/frontend/src/app/Vacctination/SecondVForm.js
+++ src/main/frontend/src/app/Vacctination/SecondVForm.js
@@ -50,8 +50,16 @@ export default class SecondVForm extends Component {
                                 title='Время приема'
                                 value={fields.time ? fields.time.value : undefined}
                             >
+                                <option value="8:00">8:00</option>
+                                <option value="9:00">9:00</option>
                                 <option value="10:00">10:00</option>
                                 <option value="11:00">11:00</option>
+                                <option value="12:00">12:00</option>
+                                <option value="13:00">13:00</option>
+                                <option value="14:00">14:00</option>
+                                <option value="15:00">15:00</option>
+                                <option value="16:00">16:00</option>
+                                <option value="17:00">17:00</option>
                             </select>
                         </div>
                     </div>
