bba93c759082aae461b21ab289f5fe6cc625ea82
Marina_2.0 <chursina0121@gmail.com>
2022-01-28 21:45:58 +0300

    Для красоты

--- src/main/frontend/src/app/Carousel/Carousel.js
+++ src/main/frontend/src/app/Carousel/Carousel.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import photo0 from "../../../public/static/images/bnr0.png";
 import photo1 from "../../../public/static/images/bnr1.png";
 import photo2 from "../../../public/static/images/bnr2.png";
 import photo3 from "../../../public/static/images/bnr3.png";
@@ -12,12 +13,15 @@ export default class CarouselPage extends Component {
                 showArrows={true}
                 autoPlay={true}
                 interval={5000}
-                style={{width: 1127}}
+                style={{width: 1128}}
                 showThumbs={false}
                 showStatus={false}
                 className={"mainCarousel"}
                 infiniteLoop={true}
             >
+                <div>
+                    <img alt="реклама" src={photo0}/>
+                </div>
                 <div>
                     <img alt="реклама" src={photo1}/>
                 </div>
